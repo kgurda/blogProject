e472bf3404a7dd7ae9458b6849d3e690e929411c
Katarzyna Gurda <gurda.kasia@gmail.com>
2017-05-30 15:32:34 +0200

    added Blog module

---
+++ module/Blog/src/Module.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Blog;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    // Autoload all classes from namespace 'Blog' from '/module/Blog/src/Blog'
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                )
+            )
+        );
+    }
+}
